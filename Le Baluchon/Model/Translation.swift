2892e61e603ad031fac517888425755fbff21faa
NicolasJaved <nicolas.javed512@gmail.com>
2018-06-20 09:05:50 +0200

    Creation of traduction branch to work of the traduction tab of the app creation of translation class creation of testTranslation

---
+++ Le Baluchon/Model/Translation.swift
@@ -0,0 +1,13 @@
+//
+//  Translation.swift
+//  Le Baluchon
+//
+//  Created by Nicolas on 20/06/2018.
+//  Copyright © 2018 Nicolas. All rights reserved.
+//
+
+import Foundation
+
+class Translation : ApiQuery {
+    
+}
---
+++ Le BaluchonTests/TestTranslation.swift
@@ -0,0 +1,16 @@
+//
+//  TestApiquery.swift
+//  Le BaluchonTests
+//
+//  Created by Nicolas on 28/05/2018.
+//  Copyright © 2018 Nicolas. All rights reserved.
+//
+
+import XCTest
+@testable import Le_Baluchon
+
+class TestTranslation: XCTestCase {
+
+    func test
+
+}
